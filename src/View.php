d9958f637569896438c9eb85aa87522fad4ffdb3
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-29 12:29:33 +0200

    Testcases gefixt

--- src/View.php
+++ src/View.php
@@ -29,7 +29,11 @@ class View extends Smarty
         if(defined('DRIPS_TMP')){
             $this->tmp_dir = DRIPS_TMP.'/'.$this->tmp_dir;
         }
-        $this->setTemplateDir(DRIPS_SRC.'/views/');
+        if(defined('DRIPS_SRC')){
+            $this->setTemplateDir(DRIPS_SRC.'/views/');
+        } else {
+            $this->setTemplateDir($this->tmp_dir.'/templates/');
+        }
         $this->setCompileDir($this->tmp_dir.'/templates_c/');
         $this->setConfigDir($this->tmp_dir.'/configs/');
         $this->setCacheDir($this->tmp_dir.'/cache/');
